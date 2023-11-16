41c95a30005b4c1a68acaa839a71ee8dff3e5774
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-11-16 13:23:55 -0800

    free

---
+++ stack_free.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+ * stack_free: free all nodes
+ * @stack: pointer to linked list
+ * Return: nothing
+ */
+void stack_free(stack_t *stack)
+{
+	stack_t *temp = stack;
+
+	while(stack)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
+}
